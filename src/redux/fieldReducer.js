1e6c33497166043cb47ea0929a35c014833c7538
Eksuzyan <vita.eksuzyan@bk.ru>
2023-02-28 15:07:04 +0300

    create the empty field function

--- src/redux/fieldReducer.js
+++ src/redux/fieldReducer.js
@@ -1,5 +1,7 @@
+const CREATE_EMPTY_FIELD = 'CREATE_EMPTY_FIELD';
+
 let initialState = {
-    field: [],
+    field: [[]],
     minesCount: 40,
     fieldSize: [16, 16],
     // состояние игры: 1 - только началась, 2 - игра идет, 3 - поражение, 4 - победа
@@ -7,5 +9,25 @@ let initialState = {
 };
 
 export const fieldReducer = (state = initialState, action) => {
-    return state;
+    switch(action.type) {
+        case CREATE_EMPTY_FIELD:
+            let newField = [];
+            let row = [];
+            let key = 1
+            for (let i = 0; i < state.fieldHeight; i++) {
+                for (let j = 0; j < state.fieldWidth; j++) {
+                    // ключ type: 1 - пустая ячейка, 2 - цифра, обозначающая кол-во ячеек рядом, 3 - мина
+                    // ключ coors - координаты ячейки
+                    row.push({key: '0' + key, coors: [i, j], type: 1, close: true })
+                    key++
+                };
+                newField.push(row);
+                row = [];
+            };
+            return { ...state, field: newField };
+        default:
+            return state;
+    }
 };
+
+
