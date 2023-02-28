b4452b8e516d5e4c3a74612f2d16679b90d19b9a
Eksuzyan <vita.eksuzyan@bk.ru>
2023-02-28 15:47:28 +0300

    to fill mines field function

--- src/redux/fieldReducer.js
+++ src/redux/fieldReducer.js
@@ -1,8 +1,12 @@
+import lodash from 'lodash';
 const CREATE_EMPTY_FIELD = 'CREATE_EMPTY_FIELD';
+const FILL_FIELD = 'FILL_FIELD';
 
 let initialState = {
-    field: [[]],
+    emptyField: [],
+    field: [],
     minesCount: 40,
+    minesCoors: [],
     fieldSize: [16, 16],
     // состояние игры: 1 - только началась, 2 - игра идет, 3 - поражение, 4 - победа
     gameState: 1
@@ -16,15 +20,45 @@ export const fieldReducer = (state = initialState, action) => {
             let key = 1
             for (let i = 0; i < state.fieldHeight; i++) {
                 for (let j = 0; j < state.fieldWidth; j++) {
-                    // ключ type: 1 - пустая ячейка, 2 - цифра, обозначающая кол-во ячеек рядом, 3 - мина
+                    // ключ type: 1 - пустая ячейка; 2 - цифра, обозначающая кол-во ячеек рядом; 3 - мина
                     // ключ coors - координаты ячейки
                     row.push({key: '0' + key, coors: [i, j], type: 1, close: true })
-                    key++
+                    key++;
                 };
                 newField.push(row);
                 row = [];
             };
-            return { ...state, field: newField };
+            return { ...state, emptyField: newField };
+        case FILL_FIELD:
+            let minesCoors = [];
+            let usedCoors = [];
+
+            function getRandomMineCoors() {
+                min = Math.ceil(0);
+                max = Math.floor(15);
+                let i = 0;
+                let j = 0;
+                i = Math.floor(Math.random() * (max - min + 1)) + min;
+                j = Math.floor(Math.random() * (max - min + 1)) + min;
+                let coors = `${i} ${j}`;
+                if(usedCoors.includes(coors)) {
+                    getRandomMineCoors();
+                } else {
+                    usedCoors.push(coors);
+                    return [i, j];
+                };
+              };
+
+              let fillField = lodash.cloneDeep(state).emptyField;
+
+              for(let i = 0; i < state.minesCount; i++) {
+                let coors = minesCoors.push(getRandomMineCoors());
+                minesCoors.push(coors);
+                fillField[minesCoors[0]][minesCoors[1]].type = 3;
+              };
+
+              return { ...state, field: fillField, minesCoors: minesCoors };
+
         default:
             return state;
     }
